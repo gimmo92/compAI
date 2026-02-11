a8fe72f9a587d631aee5886fb81e9ae03aec69e4
Gianmarco Basso <gianmarcobasso01@gmail.com>
2026-02-11 09:59:54 +0100

    a

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,6 @@ import Audit from '../pages/Audit.vue'
 import ProfileDetail from '../pages/ProfileDetail.vue'
 import CompetitorAnalysis from '../pages/CompetitorAnalysis.vue'
 import RoleCreator from '../pages/RoleCreator.vue'
-import TopPerformers from '../pages/TopPerformers.vue'
 import CompetitorManager from '../pages/CompetitorManager.vue'
 
 const routes = [
@@ -16,7 +15,6 @@ const routes = [
   { path: '/ai-suggestions', name: 'ai-suggestions', component: AISuggestions },
   { path: '/profiles/:id', name: 'profile-detail', component: ProfileDetail },
   { path: '/budget', name: 'budget', component: Budget },
-  { path: '/top-performers', name: 'top-performers', component: TopPerformers },
   { path: '/competitor', name: 'competitor', component: CompetitorAnalysis },
   { path: '/competition/add', name: 'competitor-add', component: CompetitorManager },
   { path: '/new-role', name: 'new-role', component: RoleCreator },
